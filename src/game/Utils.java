309a0cf73f39fa6aeb9e676ba5286cc695f00044
Samuel Klein <samuel.klein@online.de>
2017-11-17 12:52:51 +0100

    added utils class

---
+++ src/game/Utils.java
@@ -0,0 +1,30 @@
+package game;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+
+    public static String getBinaryString(Long l){
+        String padding = "0000000000000000000000000000000000000000000000000000000000000000";
+        String result = padding + Long.toBinaryString(l);
+        result = result.substring(result.length() - 64, result.length());
+        return result;
+    }
+
+    public static String getDateString(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public static void printInfo(String s){
+        System.out.println("[ INFO ]("+getDateString()+")\t"+s);
+    }
+
+    public static void printError(String s){
+        System.out.println("[ ERROR ]("+getDateString()+")\t"+s);
+    }
+
+}
